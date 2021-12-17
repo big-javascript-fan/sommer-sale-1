a45dc9687518637a13230efc03f97de85faf2730
max <maxall4@protonmail.com>
2021-12-17 11:39:43 -0800

    Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import Project from "./pages/project";
+import { Link, Route } from "wouter";
+import Mint from "./pages/mint";
+import Home from "./pages/home";
 
 function App() {
-  return <Project />;
+  return (
+    <>
+      <Route path="/project/:id" component={Project} />
+      <Route path="/mint" component={Mint} />
+      <Route path="/" component={Home} />
+    </>
+  );
 }
 
 export default App;
--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -2,13 +2,23 @@ import CheckoutBar from "../components/CheckoutBar";
 import { useWeb3React, UnsupportedChainIdError } from "@web3-react/core";
 import { useRecoilState } from "recoil";
 import { chainState } from "../store";
+import { useRoute } from "wouter";
 
 const Project = () => {
   const [chain] = useRecoilState(chainState);
+  const [match, params] = useRoute("/project/:id");
+  if (params == null) {
+    return (
+      <div>
+        <h1>Project not found</h1>
+      </div>
+    );
+  }
   return (
     <div>
       <h3>Project name...</h3>
       <h2>Chain: {chain.chain}</h2>
+      <h2>ID: {params.id}</h2>
       <CheckoutBar />
     </div>
   );
