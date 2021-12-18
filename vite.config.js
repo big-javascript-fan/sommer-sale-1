24a53b3dabb63781ad1e3da636211d973005fd92
max <maxall4@protonmail.com>
2021-12-18 09:57:13 -0800

    Babel

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import babel from "vite-babel-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +13,5 @@ export default defineConfig({
       https: "https-browserify",
     },
   },
-  plugins: [react()],
+  plugins: [react(), babel()],
 });
